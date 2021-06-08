bc147009f573a6d13fec02982e9f87c6f39269ac
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2021-06-08 13:05:02 +0900

    2021-06-08-TIL

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 
 김민태의 프론트엔드 아카데미를 수강하면서 실습한 것들입니다.
 
+
+
+## 새롭게 배운점 및 활용할 수 있을만한 포인트
+
+### 2021-06-08
+
+```javascript
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'  // 기본 url
+
+ajax.open('GET', CONTENT_URL.replace('@id', id), false) // ajax로 데이터를 전송받을때 활용하는 url을 replace로 재설정한다.
+```
+
+우리가 중복적으로 사용하게될 url에서 우리가 바꿔서 사용해야하는 id부분을 보통 제외하고 사용했다. 하지만 @id로 저장을 한후에 그 값을 replace로 대체하는 방법을 이용하면 아무래도 기존 content_url에 대해 누가 보더라도 id라는 변수를 통해서 받아 온다는 것을 알 수 있으니 좀 더 가독성있다는 생각이 들었습니다. 
+
+
+
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -118,20 +118,37 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
+var container = document.getElementById('root');
 var ajax = new XMLHttpRequest();
+var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
 var newsFeed = JSON.parse(ajax.response);
 var ul = document.createElement('ul');
+window.addEventListener('hashchange', function () {
+  var id = location.hash.substr(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+  var newsContent = JSON.parse(ajax.response);
+  console.log(newsContent);
+  var title = document.createElement('h1');
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+});
 
 for (var i = 0; i < 10; i++) {
   var li = document.createElement('li');
-  li.innerHTML = newsFeed[i].title;
+  var a = document.createElement('a');
+  a.href = "#".concat(newsFeed[i].id);
+  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
 },{}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -160,7 +177,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62189" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59092" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
