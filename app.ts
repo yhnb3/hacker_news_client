3b415df37d36c3091a587cedb63119adc9706336
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2021-06-24 19:56:04 +0900

    make type with interface

--- app.ts
+++ app.ts
@@ -1,29 +1,29 @@
-type Store = {
+interface Store {
   currentPage: number;
   feeds:  NewsFeed[];
 }
 
-type News = {
-  id: number;
-  time_ago: string;
-  title: string;
-  url: string;
-  user: string;
-  content: string;
+interface News {
+  readonly id: number;
+  readonly time_ago: string;
+  readonly title: string;
+  readonly url: string;
+  readonly user: string;
+  readonly content: string;
 }
-type NewsFeed = News  & {
-  comments_count: number;
-  points: number
+interface NewsFeed extends News{
+  readonly comments_count: number;
+  readonly points: number
   read?: boolean;
 }
 
-type NewsDetail = News & {
-  comments:  [];
+interface NewsDetail extends News {
+  readonly comments:  [];
 }
 
-type NewsComment = News & {
-  comments: [];
-  level: number;
+interface NewsComment extends News {
+  readonly comments: [];
+  readonly level: number;
 }
 
 const container : HTMLElement | null = document.getElementById('root')
